e6889d0c806237a54f2eadc3bb448491bd62eb01
cbarrientos <cbarrientos@ias.com.co>
2021-03-23 15:59:32 -0500

    Working on queries

--- src/main/java/com/example/demo/repository/ServicesRepository.java
+++ src/main/java/com/example/demo/repository/ServicesRepository.java
@@ -2,14 +2,13 @@ package com.example.demo.repository;
 
 import com.example.demo.model.ServiceReport;
 import com.example.demo.model.TechnicianId;
-import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 
-
 public interface ServicesRepository {
-    List<ServiceReport> getReportsByTechnicianId(TechnicianId technicianId, int weekNumber);
+    List<ServiceReport> getReportsByTechnicianId(TechnicianId technicianId, LocalDate weekDate);
 
     ServiceReport storeServiceReport(ServiceReport serviceReport);
 }
--- src/main/java/com/example/demo/repository/SqlServicesRepository.java
+++ src/main/java/com/example/demo/repository/SqlServicesRepository.java
@@ -2,12 +2,22 @@ package com.example.demo.repository;
 
 import com.example.demo.model.ServiceReport;
 import com.example.demo.model.TechnicianId;
+import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementSetter;
+import org.springframework.jdbc.core.RowCallbackHandler;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.TemporalField;
+import java.time.temporal.WeekFields;
 import java.util.List;
+import java.util.Locale;
+import java.util.Objects;
 
 @Repository
 public class SqlServicesRepository implements ServicesRepository {
@@ -17,13 +27,21 @@ public class SqlServicesRepository implements ServicesRepository {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-
     @Override
-    public List<ServiceReport> getReportsByTechnicianId(TechnicianId technicianId, int weekNumber) {
-        jdbcTemplate.query("SELECT * FROM SERVICES WHERE technicianId = ? AND initDateTime > ? AND endDateTime < ?", ps -> {}, (rs, rowNum) -> {
+    public List<ServiceReport> getReportsByTechnicianId(TechnicianId technicianId, LocalDate weekDate) {
+        PreparedStatementSetter preparedStatementSetter = ps -> {
+            ps.setString(1, technicianId.getValue());
 
-
-        });
+        };
+        RowMapper<ServiceReport> rowMapper = (rs, rowNum) -> {
+            return null;
+        };
+        String query = "SELECT * FROM SERVICES WHERE technicianId = ? AND initDateTime > ? AND endDateTime < ?";
+        jdbcTemplate.query(
+                query,
+                preparedStatementSetter,
+                rowMapper
+        );
         return null;
     }
 
@@ -38,5 +56,15 @@ public class SqlServicesRepository implements ServicesRepository {
             ps.setTimestamp(5, Timestamp.valueOf(serviceReport.getEndDate()));
         });
         return serviceReport;
+    }
+
+
+    private Pair<LocalDateTime, LocalDateTime> getWeekDates(LocalDate localDate) {
+        Objects.requireNonNull(localDate);
+        TemporalField weekOfYear = WeekFields.of(Locale.getDefault()).weekOfYear();
+        localDate.get(weekOfYear);
+        LocalDateTime initialDate = null;
+        LocalDateTime endDate = null;
+        return Pair.of(initialDate, endDate);
     }
 }
